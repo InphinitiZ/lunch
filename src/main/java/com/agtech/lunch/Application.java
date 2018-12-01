ca236306d7674b6ff41e860a3724a7a9fc982e3c
InphinitiZ <zuoxin27@126.com>
2018-12-01 21:05:07 +0800

    Update Application.java

--- src/main/java/com/agtech/lunch/Application.java
+++ src/main/java/com/agtech/lunch/Application.java
@@ -2,6 +2,8 @@ package com.agtech.lunch;
 
 import java.util.Arrays;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
